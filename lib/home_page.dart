6211e99ffa24f6bcebe7843766821ac989af1001
jozadaque <jozadaquesousa@gmail.com>
2022-02-12 20:35:38 -0300

    Criação de Appbar com itens

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,6 +7,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          title: SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('Atividades'),
+                Text('Fluttando Masterclass'),
+              ],
+            ),
+          ),
+          leading: Image.asset('assets/images/logo.png'),
+          actions: const [Icon(Icons.dark_mode)],
+        ),
         body: Container(),
       ),
     );
