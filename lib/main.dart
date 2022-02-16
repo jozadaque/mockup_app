da99918264eae5e8b62cd025fa1be499e7ef5829
jozadaque <jozadaquesousa@gmail.com>
2022-02-16 00:28:47 -0300

    Exclusão de Pastas Vazias

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mockup_app/pages/homepage/home_page.dart';
-import 'package:mockup_app/pages/splash/splash_page.dart';
+import 'package:mockup_app/pages/home_page.dart';
+import 'package:mockup_app/pages/splash_page.dart';
 
 void main() {
   runApp(const MockupApp());
